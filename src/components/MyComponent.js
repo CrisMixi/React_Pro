f3f3db4ccc4562b615512e0325500bb558cf7812
Bao <vuthebao773@gmail.com>
2023-01-24 09:06:12 +0700

    #16 Component

---
+++ src/components/MyComponent.js
@@ -0,0 +1,18 @@
+// class component
+// function component
+
+import React from "react";
+
+class MyComonent extends React.Component {
+    // JSX
+    render() {
+        return (
+            <div>
+                component của tao
+                {Math.random()}
+            </div>
+        );
+    }
+}
+
+export default MyComonent;
