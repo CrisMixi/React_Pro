ab70c36058e08afe59e1940f75d514981d431bec
Bao <vuthebao773@gmail.com>
2023-01-28 14:48:39 +0700

    #19 DOM Event

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -11,6 +11,15 @@ class MyComonent extends React.Component {
         age: 100
       };
     
+
+      onClickTest = (event) => {
+        // console.log("Pro vip");
+        console.log(event);
+      };
+
+      onClickTest_2 = (event) => {
+        console.log(event);
+      }
     // JSX
     render() {
         return (
@@ -20,6 +29,11 @@ class MyComonent extends React.Component {
                 <div>
                   {"this age: "+ this.state.age} and address của tao: {this.state.address};  
                 </div>
+                <button onClick={this.onClickTest}>click vào đây</button> {/* this.onClickTest - Cần phải dùng this. vì 
+                để cho nó biết là mình đang cần xử lý {hàm} thuộc một biến {object}.
+                onClickTest do class MyComonent quản lý*/}
+
+                <button onMouseOver={this.onClickTest_2}>click mouse</button>
             </div>
         );
     }
